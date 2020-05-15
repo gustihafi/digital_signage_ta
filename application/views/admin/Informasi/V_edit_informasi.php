38d658a3908fe48e4f4abedf989c8f98f4bee143
anitanurlaila031199 <62193534+anitanurlaila031199@users.noreply.github.com>
2020-05-15 11:36:43 +0800

    Update V_edit_informasi.php

--- application/views/admin/Informasi/V_edit_informasi.php
+++ application/views/admin/Informasi/V_edit_informasi.php
@@ -1,6 +1,7 @@
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
